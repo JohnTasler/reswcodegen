a8c78b6a3b2d5566019f4c4ce8d869b692f308a7
John Tasler <taz@mysticpop.com>
2025-04-13 05:52:29 -0700

    Added DOM-based Global namespace prefixing

--- src/VSPackage/CustomTool/ICodeGenerator.cs
+++ src/VSPackage/CustomTool/ICodeGenerator.cs
@@ -1,9 +1,12 @@
-﻿namespace ChristianHelle.DeveloperTools.CodeGenerators.Resw.VSPackage.CustomTool
+﻿using System.CodeDom;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.Resw.VSPackage.CustomTool
 {
     public interface ICodeGenerator
     {
         IResourceParser ResourceParser { get; set; }
         string Namespace { get; set; }
         string GenerateCode();
+        CodeCompileUnit CodeCompileUnit { get; }
     }
 }
